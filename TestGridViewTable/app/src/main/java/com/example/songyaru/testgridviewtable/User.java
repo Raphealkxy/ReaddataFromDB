c4c8eae7c4a06e226788812cc0c8d0690ecb6b8c
Raphealkxy <1277609958@qq.com>
2017-08-26 18:09:30 +0800

    readDataFromDB

---
+++ TestGridViewTable/app/src/main/java/com/example/songyaru/testgridviewtable/User.java
@@ -0,0 +1,64 @@
+package com.example.songyaru.testgridviewtable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.collections.map.HashedMap;
+
+public class User {
+       // private int uid;
+        private String username;
+        private String password;
+        private String telephone;
+        private String Email;
+        private String userNumber;
+        private String sex;
+        
+	
+		
+		public String getSex() {
+			return sex;
+		}
+		public void setSex(String sex) {
+			this.sex = sex;
+		}
+		public String getUserNumber() {
+			return userNumber;
+		}
+		public void setUserNumber(String userNumber) {
+			this.userNumber = userNumber;
+		}
+//		public int getUid() {
+//			return uid;
+//		}
+//		public void setUid(int uid) {
+//			this.uid = uid;
+//		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getTelephone() {
+			return telephone;
+		}
+		public void setTelephone(String telephone) {
+			this.telephone = telephone;
+		}
+		public String getEmail() {
+			return Email;
+		}
+		public void setEmail(String email) {
+			Email = email;
+		}
+	
+        
+        
+}
